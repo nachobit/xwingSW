df1a7e722e0e79163c3fca69e7365a15870bb057
nachobitPC <nachotempus@gmail.com>
2024-10-19 12:49:55 +0200

    sort pom.xml and added dependencies

--- demoCRUD/src/main/java/com/xwingSW/demoCRUD/DemoCrudApplication.java
+++ demoCRUD/src/main/java/com/xwingSW/demoCRUD/DemoCrudApplication.java
@@ -2,8 +2,10 @@ package com.xwingSW.demoCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DemoCrudApplication {
 
 	public static void main(String[] args) {
